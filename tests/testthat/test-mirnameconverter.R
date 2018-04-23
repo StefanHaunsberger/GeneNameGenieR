10945161498793e0fbc964041a0ef9647e088810
stefanhaunsberger <stefan.haunsberger@gmail.com>
2018-04-23 16:59:09 +0100

    refactor(tests): Add test case for miRNA name conversion

--- tests/testthat/test-mirnameconverter.R
+++ tests/testthat/test-mirnameconverter.R
@@ -5,7 +5,7 @@ testthat::context("mirnameconversion");
 
 gng = GeneNameGenieR();
 
-testthat::test_that("", {
+testthat::test_that("convertToCurrentMirbaseVersion of 'hsa-miR-29a' and get some metadata", {
 
     inputIds = 'hsa-miR-29a';
     target = data.frame(
@@ -18,4 +18,18 @@ testthat::test_that("", {
         stringsAsFactors = FALSE);
 
     testthat::expect_equal(convertToCurrentMirbaseVersion(gng, inputIds, metadata = c('nExperiments', 'evidenceType')), target);
+})
+
+testthat::test_that("convertMatureMirnasToVersions(gng, 'hsa-miR-29a', c(17, 21, 22))", {
+
+    inputIds = 'hsa-miR-29a';
+    versions = c(17, 21, 22)
+    target = data.frame(
+        InputId = inputIds,
+        MatureAccession = rep("MIMAT0000086", 3),
+        miRBaseVersion = c("17", "21", "22"),
+        TargetMirna = c("hsa-miR-29a", "hsa-miR-29a-3p", "hsa-miR-29a-3p"),
+        stringsAsFactors = FALSE);
+
+    testthat::expect_equal(convertMatureMirnasToVersions(gng, inputIds, c(17, 21, 22)), target);
 })
